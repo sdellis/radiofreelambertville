7b30dfa9bf9f37e7add05dcfda6de9cbecdf72f8
Shaun Ellis <shaun@sdellis.com>
2020-12-14 08:38:16 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="WRFL logo" src="./assets/wrfl-logo.png">
+    <!-- <vue-audio
+      audio-source="http://wrfl.ddns.net:8000/_a"
+    ></vue-audio> -->
+
+      <div v-for="show in radio.currentShow" :key="show.name">
+        <h2>{{ show.name }}</h2>
+      </div>
+      <h3>Current Track:</h3>
+      <p>{{ radio.current.name }}
+        <br/> by {{ radio.current.metadata.artist_name }}
+        <br/>
+        <em>({{ radio.current.metadata.album_title }})</em></p>
+    <p>
+    <audio controls src="http://wrfl.ddns.net:8000/_a"></audio>
+    </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data: function() {
+    return {
+      radio: {
+        currentShow:[{name:'loading...'}],
+        current: {
+          metadata: { artist_name: '', album_title: '' }
+        }
+      }
+    }
+  },
+  methods: {
+  },
+  mounted: function () {
+    //Initial Load
+    // this.loadData();
+    //Run every 30 seconds
+    var that = this
+    var loadData = function(){
+        fetch("http://wrfl.ddns.net/api/live-info")
+        .then(response => response.json())
+        .then(radio => (
+          that.radio = radio
+        ));
+        // Do stuff
+        setTimeout(loadData, 5000);
+    };
+    setTimeout(loadData,5000);
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAudio from 'vue-audio-better'
+
+Vue.use(VueAudio)
 
 Vue.config.productionTip = false
 
