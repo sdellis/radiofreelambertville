749cb56c546e5b03ca7d115a415b82594ebd30ba
Shaun Ellis <shaun@sdellis.com>
2021-01-02 08:17:38 -0500

    fixes safari bug and html encoding bug

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div id="app">
     <img alt="WRFL logo" src="./assets/wrfl-logo.png">
-    <!-- <vue-audio
-      audio-source="http://wrfl.ddns.net:8000/_a"
-    ></vue-audio> -->
-
-      <div v-for="show in radio.currentShow" :key="show.name">
-        <h2>{{ show.name }}</h2>
-      </div>
-      <h3>Current Track:</h3>
-      <p>{{ radio.current.name }}
-        <br/><span v-if="metadata.artist_name.length"> by {{ metadata.artist_name }}</span>
-        <br/>
-        <em v-if="metadata.album_title.length">({{ metadata.album_title }})</em></p>
-    <p>
-    <!-- <audio controls src="http://wrfl.ddns.net:8000/_a"></audio> -->
-    <audio width="300" height="32" controls="controls">
-      <source src="http://wrfl.ddns.net:8000/_b" type="audio/mpeg" />
-      <source src="http://wrfl.ddns.net:8000/_a" type="audio/ogg" />
-    </audio>
-    </p>
+    <div v-if="protocol == 'http'">
+        <div v-for="show in radio.currentShow" :key="show.name">
+          <h2>{{ decodeHtml(show.name) }}</h2>
+        </div>
+        <h3>Current Track:</h3>
+        <p>{{ radio.current.name }}
+          <br/><span v-if="metadata.artist_name.length"> by {{ decodeHtml(metadata.artist_name) }}</span>
+          <br/>
+          <em v-if="metadata.album_title.length">({{ decodeHtml(metadata.album_title) }})</em></p>
+      <p>
+      <!-- <audio controls src="http://wrfl.ddns.net:8000/_a"></audio> -->
+      <audio width="300" height="32" controls="controls">
+        <source src="http://wrfl.ddns.net:8000/_a" type="audio/mpeg" />
+        <source src="http://wrfl.ddns.net:8000/_b" type="audio/ogg" />
+      </audio>
+      </p>
+    </div>
+    <div v-else>
+      <h1>Oops!</h1>
+      <p>Radio Free Lambertville isn't sending anything secure over the interwebs, so we are only broadcasting from HTTP (not HTTPS).</p>
+      <p>Just click here and you'll be good: <a href="http://radiofreelambertville.com">http://radiofreelambertville.com</a></p>
+    </div>
   </div>
 </template>
 
@@ -46,10 +49,20 @@ export default {
     metadata: function () {
       // `this` points to the vm instance
       return ( typeof(this.radio.current.metadata) != "undefined" ) ? this.radio.current.metadata : { artist_name: '', album_title: '' };
+    },
+    protocol: function () {
+      // check the protocol and set appropriately
+      var url = window.location.href
+      var arr = url.split(":")
+      return arr[0]
     }
   },
   methods: {
-
+    decodeHtml: function (html) {
+      var txt = document.createElement("textarea");
+      txt.innerHTML = html;
+      return txt.value;
+    }
   },
   mounted: function () {
     //Initial Load
